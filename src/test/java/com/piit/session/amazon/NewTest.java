550c9710e7cfc33a2eaa5fc1958cfa1366259591
ashsid1 <maurier1985@gmail.com>
2019-11-02 13:15:31 -0400

    just trying out this commit feature

--- src/test/java/com/piit/session/amazon/NewTest.java
+++ src/test/java/com/piit/session/amazon/NewTest.java
@@ -3,7 +3,7 @@ package com.piit.session.amazon;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
-
+//testing 123
 public class NewTest {
   @Test
   public void f() {
