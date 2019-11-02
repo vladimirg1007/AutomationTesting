c7f1622c7c0e60238b7524df06352201e4335257
Aminizle <57292718+Aminizle@users.noreply.github.com>
2019-11-02 10:51:20 -0400

    Add files via upload

---
+++ src/test/java/com/piit/session/amazon/Home.java
@@ -0,0 +1,10 @@
+package com.piit.session.amazon;
+
+public class Home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/com/piit/session/amazon/NewTest.java
@@ -0,0 +1,19 @@
+package com.piit.session.amazon;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class NewTest {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
