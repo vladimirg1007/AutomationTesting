ff4ca662f8a904e0792f3160bdb9de8362ac1391
Aminizle <57292718+Aminizle@users.noreply.github.com>
2019-11-04 19:39:01 -0500

    Parallel Testing chrome&firefox

---
+++ TestNG_parallel/rich.java
@@ -0,0 +1,43 @@
+package TestNG_parallel;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.testng.annotations.AfterClass;
+
+public class rich {
+  
+	public WebDriver driver;
+	
+	@Test
+  public void f() throws InterruptedException {
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//*[@id=\'tsf\']/div[2]/div[1]/div[1]/div/div[2]/input")).sendKeys("how to be rich");
+		driver.findElement(By.name("btnK")).click();
+	
+  }
+  @BeforeClass
+  public void beforeClass() {
+	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ayesha\\eclipse-workspace\\SeleniumPractice\\Driver\\chromedriver.exe");
+		driver =new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(6,TimeUnit.SECONDS);
+		driver.navigate().to("https://www.google.com/");
+		driver.manage().window().maximize();
+	  
+  }
+
+  @AfterClass
+  public void afterClass() throws InterruptedException {
+	  Thread.sleep(10000);
+	  driver.quit();
+	  
+  }
+
+}
